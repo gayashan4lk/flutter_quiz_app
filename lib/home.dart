64687264bfcb59629f68ec20acfd2e37f1c4aebd
gayashan4lk <gayashan4lk@gmail.com>
2023-10-02 04:41:40 +0530

    home screen ui

---
+++ lib/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.deepPurple,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Image.asset(
+                  "assets/images/quiz-logo.png",
+                  width: 200,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                child: const Text(
+                  "Quiz App",
+                  style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/quiz");
+                },
+                child: const Text("Start Quiz"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
